81d8b7cf1ef07777cbf6b34078b95167f2aea08f
annabeldornan <Annabel.Dornan@student.norwichuni.ac.uk>
2024-10-17 12:01:13 +0100

    added sound duration in the console log
    yaaaaaay yoasobi :D

--- javascript.js
+++ javascript.js
@@ -1,27 +1,43 @@
-const audioPlayer = newAudio(assets/YOASOBI - Idol.mp3);
+const audioPlayer = new Audio();
 
 const playPauseButton = document.getElementById("play-button");
 
 // audioPlayer.src is the first song of the audioplayer by default
-audioPlayer.src = "assets/sound/YOASOBI - Idol.mp3";
+audioPlayer.src = "assets/yoasobi-idol.mp3";
 
+let playing = false;
 
 /**
- * if audio player is playing --> play sound
- * if audio player is ! playing --> dont play sound 
+ * if audio player is ! playing --> dont play sound
+ * if audio player is playing --> play sound 
  
  */
+
 function onPlayPauseClick(){
-    audioPlayer.play();
-    ;
-    audioPlayer.pause();
-    ;
+    if(playing) {
+        audioPlayer.pause();
+        playPauseButton.HTML = "Play";
+        playing = false;
+    } else {
+        audioPlayer.play();
+        playPauseButton.HTML = "Pause";
+        playing = true;
+    }
+    // audioPlayer.play();
+    // playPauseButton.innerHTML = "Pause";
 }
 
-// link onclick events to the onPlayPauseClick button
+/** */
+function onLoadedMetadata(){
+    console.log(audioPlayer.duration);
+}
+
+// link onclick events to relative objects
 playPauseButton.onclick = onPlayPauseClick;
+audioPlayer.onloadedmetadata = onLoadedMetadata;
 
 
-/*     \(^w^)|     */
+/*     |(^w^)/     */
+
 
 
